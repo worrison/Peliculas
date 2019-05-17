35aa3eba7bc1f834ffb780d8a7500cd7b0f42d9c
victor ordax <victorordaxantolin@gmail.com>
2019-05-17 13:51:30 +0200

    Solucionado la funcion busqueda usando la api

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //import films from './films.js'
 const URL_IMAGE="http://image.tmdb.org/t/p/w185/";
-
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '9ec2c792cfa94b0acb15cb59b0051990';
 const API_POPULAR_URL = 'movie/popular';
 const API_CATEGORIES = 'genre/movie/list';
+let peliculas;
 /*https://api.themoviedb.org/3/genre/movie/list?api_key=9ec2c792cfa94b0acb15cb59b0051990*/
 
 
@@ -12,7 +12,6 @@ window.addEventListener('load',onLoad);
 function onLoad()
 {
   let generos;
-  let peliculas;
   let btnbuscar=document.querySelector('#btnBuscar');
   btnbuscar.addEventListener('click',buscar);
   //showFilms(films);
@@ -21,7 +20,6 @@ function onLoad()
     //console.log(response.data.results);
     peliculas=response.data.results;
     showFilms(peliculas);
-
     //generos 
       axios.get(API_URL + API_CATEGORIES + '?api_key='+ API_KEY).then((response)=>
       {
@@ -32,7 +30,7 @@ function onLoad()
             return generos.find((genero)=>genero.id===id);
           });
           peliculas.genres=arrayGeneros;//asigna la propiedad genres cn los datos de arry generos
-          peliculas["poster_path"]=URL_IMAGE + peliculas["poster_path"];
+          //peliculas["poster_path"]=URL_IMAGE + peliculas["poster_path"];
           peliculas.stars=Math.round(peliculas["vote_average"]/2);
           return peliculas;
         });
@@ -71,7 +69,8 @@ function showFilms(filmsToPrint)
 function buscar()
 {
   let textoBuscar=document.querySelector('#textBuscar').value;
-  let resultBusqueda=films.filter(pelicula =>pelicula.title.toLowerCase().includes(textoBuscar.toLowerCase()));
+  let resultBusqueda=peliculas.filter(pelicula =>pelicula.title.toLowerCase().includes(textoBuscar.toLowerCase()));
+  console.log(resultBusqueda);
   showFilms(resultBusqueda);
 }
 
