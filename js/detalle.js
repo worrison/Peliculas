91ff729a4e3a920a6a88c4068de5c44953b5321b
victor ordaxm <victorordaxantolin@gmail.com>
2019-05-19 15:23:50 +0200

    estructura de pagina detalle y carga de imagen como fondo del div don de se va mostrando los detalles de la pelicula

--- js/detalle.js
+++ js/detalle.js
@@ -1,4 +1,6 @@
-const URL_IMAGE = "http://image.tmdb.org/t/p/w185/";
+const URL_IMAGE_LARGE = "http://image.tmdb.org/t/p/w600_and_h900_bestv2/";//ampliada
+const URL_IMAGE_BACKGROUND = "http://image.tmdb.org/t/p/w1400_and_h450_face/";//imagen de fondo
+const URL_IMAGE = "http://image.tmdb.org/t/p/w300_and_h450_bestv2/";//Imagen normal para el detalle
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '9ec2c792cfa94b0acb15cb59b0051990';
 const API_DETAILS = 'movie/';
@@ -24,14 +26,17 @@ function onLoad()
 
 
 function showDetails()
-{
-  let body = document.querySelector('body');
+{    
+  let detailFilm = document.querySelector('#detailFilm');
+  detailFilm.style.backgroundImage='radial-gradient(circle at 20% 50%, rgba(3.14%, 14.51%, 40.39%, 0.98) 0%, rgba(3.14%, 14.51%, 40.39%, 0.88) 100%),url('+URL_IMAGE_BACKGROUND+''+film.poster_path+')';
+
   let divFilm = document.createElement('article');
+    divFilm.setAttribute("class", "container");//añado la clase container al elemento article
     divFilm.innerHTML = `
-  <img src="${URL_IMAGE}${film.poster_path}" alt="${film.title}">
-    <p class="title">${film.title}</p>
+    <div class="img-fluid"><img src="${URL_IMAGE}${film.poster_path}" alt="${film.title}"></div>
+    <div class="detail"><h1 class="title">${film.title}<span>${film.release_date.substr(0,4)}</span></h1>
     <p class="ratio">${film.vote_average}</p>
-    <p class="overview">${film.overview}</p>
+    <p class="overview">${film.overview}</p></div>
     `;
-    body.appendChild(divFilm);
+    detailFilm.appendChild(divFilm);
 }
